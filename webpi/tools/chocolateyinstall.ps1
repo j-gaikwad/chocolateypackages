58545a0018f5a48a64852511b456778d540f0692
Jonathan Gaikwad <jgaikwad@educationdynamics.com>
2020-08-12 11:15:47 -0500

    first commit

---
+++ webpi/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://go.microsoft.com/fwlink/?LinkId=287165' # download url, HTTPS preferred
+$url64      = 'https://go.microsoft.com/fwlink/?LinkId=287166' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI' #only one of these: exe, msi, msu
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'webpi*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
